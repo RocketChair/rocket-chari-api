5b86390a1e75107a9b9f388a14a9dbcf1a1c188f
Konrad Stasiuk <konrad.stasiuk@edge1s.com>
2017-04-08 10:53:53 +0200

    init api

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const app = require("express")();
 const WebSocket = require("ws");
 
+const PORT_NUMBER = process.env.PORT || 8080;
+
 const wss = new WebSocket.Server({
   perMessageDeflate: false,
-  port: 8080
+  port: PORT_NUMBER
 });
 
 wss.on("connection", function(socket) {
   socket.on("message", function(msg) {
     console.log(msg);
-
     socket.send(msg.toUpperCase());
   });
 });
 
-app.listen(3000, () => {
-  console.log("listen at port 3000");
+app.listen(PORT, () => {
+  console.log("listen at port " + PORT_NUMBER);
 });
